bb9b187418bdbbcd5cdb50e88e7c707ffa9a936e
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-03-15 18:08:59 -0300

    Cadastro concluído

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import RegisterScreen from "./Pages/RegisterScreen";
 import Habits from "./Pages/Habits";
 import Today from "./Pages/Today";
 import History from "./Pages/History";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 
 export default function App() {
 
   return (
-    <div>
-      <LoginScreen />
-      {/* <RegisterScreen /> */}
-      {/* <Habits /> */}
-      {/* <Today /> */}
-      {/* <History /> */}
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginScreen />}/>
+        <Route path="/cadastro" element={<RegisterScreen />}/>
+        <Route path="/habitos" element={<Habits />}/>
+        <Route path="/hoje" element={<Today />}/>
+        <Route path="/historico" element={<History />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/Pages/RegisterScreen.js
+++ src/Pages/RegisterScreen.js
@@ -1,17 +1,53 @@
 import styled from "styled-components";
 import LogoLoginRegister from "../Components/LogoLoginRegister";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { URL } from "../Components/Constants/URL";
+import axios from "axios";
 
 export default function RegisterScreen(){
+
+    const [emailInput,setEmailInput] = useState("");
+    const [passInput,setPassInput] = useState("");
+    const [namelInput,setNameInput] = useState("");
+    const [pictureInput,setPictureInput] = useState("");
+    const navigate = useNavigate();
+
+    function registerUser(e){
+        e.preventDefault();
+
+        const body = {
+            email: emailInput,
+            name: passInput,
+            image: pictureInput,
+            password: namelInput
+        };
+
+        axios.post(`${URL}/auth/sign-up`, body)
+            .then(res => {
+                console.log(res);
+                navigate("/hoje");
+                alert("Deu certo");
+            })
+            .catch(err => {
+                console.log(body);
+                console.log(err.response);
+                alert("Deu erro");
+            });
+    }
+
     return (
         <Container>
             <ContainerInt>
                 <LogoLoginRegister />
-                <input type="email" placeholder="email" required/>
-                <input type="password" placeholder="senha" required/>
-                <input type="text" placeholder="nome" required/>
-                <input type="text" placeholder="foto" required/>
-                <button>Cadastrar</button>
-                <p>Já tem uma conta? Faça login!</p>
+                <form onSubmit={registerUser}>
+                    <input type="email" placeholder="email" value={emailInput} onChange={e => setEmailInput(e.target.value)} required/>
+                    <input type="password" placeholder="senha" value={passInput} onChange={e => setPassInput(e.target.value)} required/>
+                    <input type="text" placeholder="nome" value={namelInput} onChange={e => setNameInput(e.target.value)} required/>
+                    <input type="text" placeholder="foto" value={pictureInput} onChange={e => setPictureInput(e.target.value)} required/>
+                    <button>Cadastrar</button>
+                    <p>Já tem uma conta? Faça login!</p>
+                </form>
             </ContainerInt>
         </Container>
     )
@@ -34,7 +70,6 @@ const ContainerInt = styled.div`
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
-    gap: 6px;
     position: fixed;
     top: 68px;
     p {
@@ -45,5 +80,13 @@ const ContainerInt = styled.div`
         font-weight: 400;
         line-height: 17px;
         text-decoration: underline;
+    }
+    form {
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 6px;
     }
 `
